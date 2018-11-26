24b30ad587f324d32cfc5194e627acddedb6427f
warryr <heartsyrop@gmail.com>
2018-11-26 17:33:22 +0300

    feat: add function makeRequest

--- youtube-client/index.js
+++ youtube-client/index.js
@@ -17,4 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
   searchInput.setAttribute('type', 'text');
   searchButton.setAttribute('type', 'submit');
   searchButton.innerText = 'Submit';
+
+  function makeRequest(e) {
+    e.preventDefault();
+    const searchValue = searchInput.value;
+    const url = `https://www.googleapis.com/youtube/v3/search?type=video&q=${searchValue}&part=snippet&maxResults=15&key=AIzaSyCmcP7pWB1HDWy2Z5PqmF4bWj4FmkSsPQM`;
+    const xhp = new XMLHttpRequest();
+    xhp.open('GET', url, true);
+    xhp.onreadystatechange = () => {
+      if (xhp.readyState === 4 && xhp.status === 200) {
+        const info = JSON.parse(xhp.responseText);
+        resultsSection.innerHTML = '';
+      }
+    };
+    xhp.send();
+  }
+
+  searchButton.addEventListener('click', makeRequest);
 });
